8376429e3ffe0f6a242f96ad79a7e041e6248e5b
network <maswaeecom@gmail.com>
2022-09-18 11:10:01 +0530

    add static

--- project/settings.py
+++ project/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
