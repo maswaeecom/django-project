7f3cbaf9ea185e08425cbb08d0ab60160d09f8c7
network <maswaeecom@gmail.com>
2022-09-18 10:59:47 +0530

    add static

--- project/urls.py
+++ project/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('userpanel.urls')),
     
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 admin.site.site_header  =  "Project"  
